b013d853f8686e0cbe04456bd8fd7d97adc036f7
Dongxu Lin <dl3636@nyu.edu>
2018-10-03 02:43:51 -0400

    draft

---
+++ PS1/src/edu/nyu/pqs1/AddressBook.java
@@ -0,0 +1,139 @@
+package edu.nyu.pqs1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+/**
+* This class is a address book. A address book contains a list of entries.
+* @author Lin Dongxu(Damien)
+*/
+public class AddressBook{
+	
+  private List<Entry> entries;
+	
+  /**
+   * creates a new address book object
+   */
+  public AddressBook() {
+    entries = new ArrayList<Entry>();
+  }
+	
+  /**
+  * add an entry into the address book.
+  * 
+  * @param an Entry e
+  * @return true when it add successfully, return false if the input entry is null or add unsuccessfully.
+  */
+  public boolean addEntry(Entry e) {
+    if (e == null) {
+	  return false;
+    }
+    return entries.add(e);
+  }
+	
+  /**
+   * remove an entry from the address book. If there are several entries in address book that are the same,
+   * only remove one of them.
+   * 
+   * @param an Entry e
+   * @return true if it remove successfully, return false if the input entry is null or removes unsuccessfully.
+   */
+  public boolean removeEntry(Entry e) {
+  	if (e == null) {
+  	  return false;
+  	}
+  	return entries.remove(e);
+  }
+  
+  /**
+   * This method use a String str as input, to search in the whole address book for any entries that contains
+   * any information same as the str.
+   * 
+   * @param a String str
+   * @return a list of Entry containing the information, if no any entry matched or input str is NULL,
+   * return an empty list.
+   */
+  public List<Entry> searchEntry(String str) {
+  	List<Entry> resList = new ArrayList<Entry>();
+  	if (str == null) {
+  	  return resList;
+  	}
+  	for (Entry e: entries) {
+  	  if (e.getEntryString().contains(str)) {
+  	    resList.add(e);
+  	  }
+  	}
+  	return resList;
+  }
+  
+  
+  /**
+   * The method serialize a address book to a local file named by input parameter fileName.
+   * @param fileName
+   * @return true if it successfully writes it. Otherwise it fails and return false because file 
+   *  already exist.
+   * @throws IOException
+   */
+  public boolean saveAddressBook(String fileName) throws IOException{
+      StringBuilder sb = new StringBuilder();
+      sb.append("name|postal|phone|email|note").append("\n");
+      for (Entry e: entries) {
+        sb.append(e.getName()).append("|");
+      	sb.append(e.getPostalAdd()).append("|");
+      	sb.append(e.getPhoneNum()).append("|");
+      	sb.append(e.getEmailAdd()).append("|");
+      	sb.append(e.getNote()).append("|");
+      	sb.append("\n");
+      }
+      
+      File f = new File(fileName);
+      if(f.exists() && !f.isDirectory()) { 
+      	return false;
+      }
+      
+      PrintStream out = new PrintStream(new FileOutputStream(fileName));
+      out.print(sb.toString());
+      out.close();
+      return true;
+  }
+  
+  /**
+   * This method read local file named fileName, then it will overwrite the address book with new data.
+   * @param fileName
+   * @throws FileNotFoundException
+   */
+  public void readAddressBook(String fileName) throws FileNotFoundException {
+  	Scanner scanner = new Scanner( new File(fileName) );
+  	String text = scanner.useDelimiter("\\A").next();
+  	scanner.close();
+  	
+  	String[] lines = text.split("\n");
+  	if (lines.length > 1) {
+  	  entries = new ArrayList<Entry>();
+  	  for (int i = 1; i < lines.length; i++) {
+  	    String[] item = lines[i].split("\\|", -1);
+		entries.add(new Entry.Builder(item[0]).setPostal(item[1]).setPhone(item[2])
+		    .setEmail(item[3]).setNote(item[4]).build());
+  	  }
+  	}
+  	
+  }
+  
+  
+  @Override
+  public String toString() {
+  	StringBuilder sb = new StringBuilder();
+  	sb.append("name|postal|phone|email|note").append("\n");
+  	for (Entry e: entries) {
+  	  sb.append(e.getEntryString()).append("\n");
+  	}
+  	return sb.toString();
+  }
+}
---
+++ PS1/src/edu/nyu/pqs1/Entry.java
@@ -0,0 +1,185 @@
+package edu.nyu.pqs1;
+
+
+/**
+*  For an entry, field name is required not empty, other fields like postalAdd can be empty.
+* 
+* @author Lin Dongxu(Damien)
+**/
+public class Entry {
+  private final String name;
+  private final String postalAdd;
+  private final String phoneNum;
+  private final String emailAdd;
+  private final String note;
+
+
+  /**
+   * inner Builder class for constructing an entry
+   */
+  public static class Builder{
+	
+	//field name required 
+    private final String name;
+	
+	//these fields are optional
+    private String postalAdd = "";
+    private String phoneNum = "";
+    private String emailAdd = "";
+    private String note = "";
+
+	
+	/**
+	 * create a new Builder object, name is required.
+	 * @param name which cannot be NULL
+	 * @throws IllegalArgumentException if input name is NULL
+	 */
+	public Builder(String name) {
+	  if (name == null) {
+	    throw new IllegalArgumentException("name cannot be NULL");
+	  }else {
+	    this.name = name;
+	  }
+	}
+	
+	/**
+	 * @param postal address value. If input is NULL, do nothing.
+	 * @return updated Builder object.
+	 */
+	public Builder setPostal(String val) {
+	  if (val == null) {
+	    return this;
+	  }
+	  postalAdd = val;
+	  return this;
+	}
+	
+	/**
+	 * The method eliminates all not digit character then update the builder.
+	 * @param phone number. If input is NULL, do nothing.
+	 * @return updated Builder object.
+	 */
+	public Builder setPhone(String num) {
+	  if (num == null) {
+        return this;
+      }
+	  phoneNum = num.replaceAll("[\\s-()]", "");
+	  return this;
+	}
+	
+	/**
+	 * @param email address value. If input is NULL, do nothing.
+	 * @return updated Builder object.
+	 */
+	public Builder setEmail(String val) {
+	  if (val == null) {
+        return this;
+      }
+	  emailAdd = val;
+	  return this;
+	}
+	
+	/**
+	 * @param note value. If input is NULL, do nothing.
+	 * @return updated Builder object.
+	 */
+	public Builder setNote(String val) {
+	  if (val == null) {
+        return this;
+      }
+	  note = val;
+	  return this;
+	}
+
+	/**
+	 * @return new Entry object
+	 */
+	public Entry build() {
+	  return new Entry(this);
+	}
+  }
+
+  private Entry(Builder builder) {
+    name = builder.name;
+	postalAdd = builder.postalAdd;
+	phoneNum = builder.phoneNum;
+	emailAdd = builder.emailAdd;
+	note = builder.note;
+  }
+
+
+  /**
+   * @return name of the entry
+   */
+  public String getName() {
+	return name;
+  }
+
+
+  /**
+   * @return postal address of the entry
+   */
+  public String getPostalAdd() {
+	return postalAdd;
+  }
+
+
+  /**
+   * @return phone number of the entry
+   */
+  public String getPhoneNum() {
+	return phoneNum;
+  }
+
+
+  /**
+   * @return email address of the entry
+   */
+  public String getEmailAdd() {
+	return emailAdd;
+  }
+
+
+  /**
+   * @return note of the entry
+   */
+  public String getNote() {
+	return note;
+  }
+
+
+  /**
+   * @return raw string of the entry
+   */
+  public String getEntryString() {
+	return name + "|" + postalAdd + "|" + phoneNum + "|" + emailAdd + "|" + note;
+  }
+
+  @Override
+  public String toString() {
+    return "Name: " + name + "|" + "postal address: " + postalAdd + "|" + "phone numer: " + phoneNum
+			+ "|" + "email address: " + emailAdd + "|" + "note: " + note;
+  }
+
+  @Override
+  public int hashCode() {
+	int result = 17;
+	int prime = 31;
+    result = prime * result + name.hashCode();
+    result = prime * result + postalAdd.hashCode();
+    result = prime * result + phoneNum.hashCode();
+    result = prime * result + emailAdd.hashCode();
+    result = prime * result + note.hashCode();
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+	if (obj.getClass() != this.getClass()) {
+	  return false;
+	}
+	return this.toString().equals(obj.toString());
+  }
+}
+
+
