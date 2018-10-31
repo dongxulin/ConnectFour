6cccf4870930067a218b453efb1302effd0fafa9
Dongxu Lin <dl3636@nyu.edu>
2018-10-30 22:58:39 -0400

    v1

--- PS2/src/edu/nyu/pqs/stopwatch/impl/StopwatchFactory.java
+++ PS2/src/edu/nyu/pqs/stopwatch/impl/StopwatchFactory.java
@@ -1,6 +1,9 @@
 package edu.nyu.pqs.stopwatch.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import edu.nyu.pqs.stopwatch.api.Stopwatch;
 
@@ -8,10 +11,10 @@ import edu.nyu.pqs.stopwatch.api.Stopwatch;
  * The StopwatchFactory is a thread-safe factory class for Stopwatch objects.
  * It maintains references to all created Stopwatch objects and provides a
  * convenient method for getting a list of those objects.
- *
+ * @author Dongxu Lin
  */
 public class StopwatchFactory {
-
+  private static Map<String, Stopwatch> stopwatchMap = new HashMap<>();
   /**
    * Creates and returns a new Stopwatch object
    * @param id The identifier of the new object
@@ -20,8 +23,17 @@ public class StopwatchFactory {
    *     already taken.
    */
   public static Stopwatch getStopwatch(String id) {
-    // replace this return statement with correct code
-    return null;
+    if (id == null || id.length() == 0) {
+      throw new IllegalArgumentException("illegal input string");
+    }
+    synchronized (stopwatchMap) {
+      if (stopwatchMap.containsKey(id)) {
+        throw new IllegalArgumentException("the id has been used");
+      }
+      Stopwatch newStopwatch = new StopwatchImplement(id);
+      stopwatchMap.put(id, newStopwatch);
+      return newStopwatch;
+    }
   }
 
   /**
@@ -30,6 +42,12 @@ public class StopwatchFactory {
    * list if no Stopwatches have been created.
    */
   public static List<Stopwatch> getStopwatches() {
-    return null;
+    synchronized (stopwatchMap) {
+      List<Stopwatch> list = new ArrayList<>();
+      for (String str: stopwatchMap.keySet()) {
+        list.add(stopwatchMap.get(str));
+      }
+      return list;
+    }
   }
 }
